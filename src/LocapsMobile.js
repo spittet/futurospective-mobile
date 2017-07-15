f4c516b987cd5229ef4ab863d3178a02bf5e483e
Sten Pittet <sten.pittet@gmail.com>
2017-07-15 19:50:49 +0100

    #18 Installed react-navigation to the project and copied the basic redux example. This provides a boilerplate that will have to be modified but currently it doesn't implement any of the feature of LoCaps.

---
+++ src/LocapsMobile.js
@@ -0,0 +1,22 @@
+/**
+ * @flow
+ */
+
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import AppReducer from './reducers';
+import AppWithNavigationState from './navigators/AppNavigator';
+
+export default class LocapsMobileApp extends React.Component {
+  store = createStore(AppReducer);
+
+  render() {
+    return (
+      <Provider store={this.store}>
+        <AppWithNavigationState />
+      </Provider>
+    );
+  }
+}
---
+++ src/reducers/index.js
@@ -0,0 +1,60 @@
+/**
+ * @flow
+ */
+
+import { combineReducers } from 'redux';
+import { NavigationActions } from 'react-navigation';
+
+import { AppNavigator } from '../navigators/AppNavigator';
+
+const firstAction = AppNavigator.router.getActionForPathAndParams('Main');
+const tempNavState = AppNavigator.router.getStateForAction(firstAction);
+const secondAction = AppNavigator.router.getActionForPathAndParams('Login');
+const initialNavState = AppNavigator.router.getStateForAction(
+  secondAction,
+  tempNavState
+);
+
+function nav(state = initialNavState, action) {
+  let nextState;
+  switch (action.type) {
+    case 'Login':
+      nextState = AppNavigator.router.getStateForAction(
+        NavigationActions.back(),
+        state
+      );
+      break;
+    case 'Logout':
+      nextState = AppNavigator.router.getStateForAction(
+        NavigationActions.navigate({ routeName: 'Login' }),
+        state
+      );
+      break;
+    default:
+      nextState = AppNavigator.router.getStateForAction(action, state);
+      break;
+  }
+
+  return nextState || state;
+}
+
+
+const initialAuthState = { isLoggedIn: false };
+
+function auth(state = initialAuthState, action) {
+  switch (action.type) {
+    case 'Login':
+      return { ...state, isLoggedIn: true };
+    case 'Logout':
+      return { ...state, isLoggedIn: false };
+    default:
+      return state;
+  }
+}
+
+const AppReducer = combineReducers({
+  nav,
+  auth
+});
+
+export default AppReducer;
