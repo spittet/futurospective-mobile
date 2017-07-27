ce6452aa65ec73539e4ecca696472c6fd799ec0d
Sten Pittet <sten.pittet@gmail.com>
2017-07-27 13:14:54 +0200

    #15 Added the Preview screen and linking react-native-video.

---
+++ src/containers/PreviewScreen.js
@@ -0,0 +1,15 @@
+/**
+ * This is the preview screen where we play the video after it's been recorded.
+ *
+ * @flow
+ */
+
+import React from 'react';
+import { View } from 'react-native';
+import RNFS from 'react-native-fs';
+
+import styles from './styles';
+
+class PreviewScreen extends React.Component {
+  
+}
