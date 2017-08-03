95471aead58480e8d7768794072f4c1a9aed6aef
Sten Pittet <sten.pittet@gmail.com>
2017-08-03 20:34:53 +0100

    #15 First pass at implementing the default directory to store videos.

--- src/LocapsMobile.js
+++ src/LocapsMobile.js
@@ -9,6 +9,9 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
 import logger from 'redux-logger';
+import RNFS from 'react-native-fs';
+
+import { CAPSULES_DIR } from './config';
 
 import AppReducer from './reducers';
 import AppWithNavigationState from './containers/AppNavigator';
@@ -16,7 +19,19 @@ import AppWithNavigationState from './containers/AppNavigator';
 export default class LocapsMobileApp extends React.Component {
   store = createStore(AppReducer, applyMiddleware(logger));
 
+  createDocumentDirectory = () => {
+    RNFS.exists(CAPSULES_DIR).then((result) => {
+      if (!result) {
+        RNFS.mkdir(CAPSULES_DIR);
+      } else {
+        console.log('dir found');
+      }
+    });
+    
+  }
+
   render() {
+    this.createDocumentDirectory();
     return (
       <Provider store={this.store}>
         <AppWithNavigationState />
--- src/config/index.js
+++ src/config/index.js
@@ -1,1 +1,4 @@
+import RNFS from 'react-native-fs';
+
+export const CAPSULES_DIR = RNFS.DocumentDirectoryPath + '/capsules';
 export const MAX_RECORDING_DURATION = 10000;
